b60f8721084d295d748bd6226071ab6ad79bc233
billah2025 <m,b,siam2008@gmail.com>
2025-04-19 17:06:20 +0600

    from linux3

--- src/app/components/ServiceSection.tsx
+++ src/app/components/ServiceSection.tsx
@@ -79,7 +79,7 @@ export default function ServiceSection() {
               <h3 className="text-xl font-semibold">Ruqyah</h3>
               <p className="text-gray-600 mt-2">Rebirth of a sunnah </p>
               <button className="mt-4 px-6 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 transition-all">
-                <a href="/learnquran">Start Listening</a>
+                <a href="/rokayah">Start Listening</a>
               </button>
             </div>
           </div>
